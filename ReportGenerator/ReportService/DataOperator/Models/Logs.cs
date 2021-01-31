40c70247c380134c66759fb38d95f93704ad42ad
Julian <julian-frech@gmx.de>
2021-01-31 18:46:27 +0100

    Version 1

---
+++ ReportGenerator/ReportService/DataOperator/Models/Logs.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataOperator.Models
+{//
+    [Table("F_SERVICE_LOGS", Schema = "logging")]
+    public class Logs
+    {
+        public int ServiceId { get; set; }
+        public string LogMessage { get; set; }
+        public string ServiceMethod { get; set; }
+        public DateTimeOffset LogTime { get; set; }
+
+        public Logs() { }
+
+        public Logs(string logString, int serviceId, string serviceMethod)
+        {
+            ServiceId = serviceId;
+            LogMessage = logString;
+            ServiceMethod = serviceMethod;
+            LogTime = DateTimeOffset.Now;
+        }
+    }
+
+    public class ApplyToDbContext_LOGS : IEntityTypeConfiguration<Logs>
+    {
+        public virtual void Configure(EntityTypeBuilder<Logs> builder)
+        {
+            builder.HasKey( x => new { x.LogMessage, x.LogTime, x.ServiceId, x.ServiceMethod});
+        }
+    }
+}
---
+++ ReportGenerator/ReportService/ReportWriter/ReportWriter.cs
@@ -0,0 +1,56 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+using FileWriter.ConfigurationOption;
+using Microsoft.Extensions.Options;
+using ReportWriter.Service;
+using ReportWriter.Service.Extensions;
+
+namespace ReportWriter
+{
+    /// <summary>
+    /// Interface of the ReportWriter class containing method to create reports.
+    /// </summary>
+    public interface IReportFile
+    {
+        /// <summary>
+        /// Access point for writing datatable input as txt delimiter formatted files.
+        /// </summary>
+        /// <param name="reportAsdataTable">Report data as datatable</param>
+        /// <param name="fileName">Name of the report</param>
+        /// <param name="targetLocationExtension">Target folder of the report file</param>
+        /// <param name="delimiter">String based delimiter i.e. ";" or ","</param>
+        /// <returns></returns>
+        Task ReportAsFileAsync(DataTable reportAsdataTable, string fileName, string targetLocationExtension, string delimiter, string headerRow);
+    }
+
+    /// <summary>
+    /// Class storing methodology to create reports.
+    /// </summary>
+    public class ReportFile : IReportFile
+    {
+        private readonly IFileWriter _fileWriter;
+
+        public ReportFile(IFileWriter fileWriter)
+        {
+            _fileWriter = fileWriter;
+        }
+
+        /// <summary>
+        /// Access point for writing datatable input as txt delimiter formatted files.
+        /// </summary>
+        /// <param name="reportAsdataTable">Report data as datatable</param>
+        /// <param name="fileName">Name of the report</param>
+        /// <param name="targetLocation">Target folder of the report file</param>
+        /// <param name="delimiter">String based delimiter i.e. ";" or ","</param>
+        /// <returns></returns>
+        public async Task ReportAsFileAsync(DataTable reportAsdataTable, string fileName, string targetLocationExtension, string delimiter, string headerRow)
+        {
+
+            await _fileWriter.StringToFile(targetLocationExtension + fileName,
+                                    reportAsdataTable.ToCsv(delimiter, headerRow)
+                                    );
+
+        }
+
+    }
+}
